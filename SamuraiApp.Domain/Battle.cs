2279927017f1438c4db73b4d65ae52bad37b3c87
mmaag <mmaag@NB21-ZDV-007>
2021-09-27 11:32:19 +0200

    Add project files.

---
+++ SamuraiApp.Domain/Battle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SamuraiApp.Domain
+{
+    public class Battle
+    {
+        public int BattleID { get; set; }
+        public string Name { get; set; }
+        public List<Samurai> Samurais { get; set; } = new List<Samurai>();
+    }
+}
